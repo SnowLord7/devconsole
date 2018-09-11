e623808d3dd9010e75f547f4ea1a0619fca9134c
SnowLord <31230715+SnowLord7@users.noreply.github.com>
2018-09-11 06:28:44 -0700

    Create Inject.js

---
+++ Inject.js
@@ -0,0 +1,6 @@
+(function() {
+    var x = document.createElement("script");
+    x.src = "https://rawgit.com/SnowLord7/devconsole/master/CustomDevConsole.js";
+    x.onload = alert("Loaded Developer Console!");
+    document.head.appendChild(x);
+})()
